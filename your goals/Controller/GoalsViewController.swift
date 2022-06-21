d4cdca321d6d6bb84e20f9668d469683c05d9a56
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-21 12:06:21 +0200

    Connect the button to the Goals VC

--- your goals/Controller/GoalsViewController.swift
+++ your goals/Controller/GoalsViewController.swift
@@ -2,8 +2,14 @@ import UIKit
 
 class GoalsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addGoalButton(_ sender: Any) {
+        print("was pressed")
     }
 }
