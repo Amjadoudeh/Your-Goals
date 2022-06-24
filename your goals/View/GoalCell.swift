1b251e6e9d04ab3fe98fb243e4fa7dd655d0f780
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-24 23:52:19 +0200

    Fix the dynamic cell and show dynamic data

--- your goals/View/GoalCell.swift
+++ your goals/View/GoalCell.swift
@@ -7,7 +7,8 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalProgressLabel: UILabel!
 
     func configureCell(description: String, type: GoalType, goalProgressAmount: Int) {
-        self.goalDescriptionLabel.text = description       self.goalTypeLabel.text = type.rawValue
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type.rawValue
         self.goalProgressLabel.text = String(describing: goalProgressAmount)
     }
 }
