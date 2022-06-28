ed4f8ab110e214d67af546187fe7d26665d068bc
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-28 13:59:05 +0200

    Create an extension to bind the Keyboard

---
+++ your goals/Extensions/UIViewExtension.swift
@@ -0,0 +1,12 @@
+import UIKit
+// an extension to bind the keyboard
+
+extension UIView {
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+
+    }
+}
