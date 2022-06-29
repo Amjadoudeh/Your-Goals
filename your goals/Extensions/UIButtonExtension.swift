3d3fa94fa628101a43d48056d6d12e491aea584b
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-29 09:29:31 +0200

    Create a UIButton extension

---
+++ your goals/Extensions/UIButtonExtension.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIButton {
+    func setSelectedColor() {
+        self.backgroundColor = UIColor(red: 109, green: 188, blue: 99, alpha: 1)
+    }
+
+    func setDeselectedColor() {
+        self.backgroundColor = UIColor(red: 178, green: 221, blue: 175, alpha: 1)
+    }
+}
