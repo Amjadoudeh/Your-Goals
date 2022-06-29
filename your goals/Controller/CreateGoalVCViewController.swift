a0470a56b41ba3f567bc288190e9cce2a901560e
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-29 10:41:28 +0200

    Add a list of bugs

--- your goals/Controller/CreateGoalVCViewController.swift
+++ your goals/Controller/CreateGoalVCViewController.swift
@@ -7,25 +7,34 @@ class CreateGoalVCViewController: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
 
+    var goalType: GoalType = .ShortTerm
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func nextButtonWasPressed(_ sender: Any) {
-        print("next")
-    }
-
-    @IBAction func shortTermButtonWasPressed(_ sender: Any) {
-        print("short")
-    }
-
-    @IBAction func longTermButtonWasPressed(_ sender: Any) {
-        print("long")
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
 
     @IBAction func backButtonWasPressed(_ sender: Any) {
         dismissDetail()
+    }
+
+    @IBAction func shortTermButtonWasPressed(_ sender: Any) {
+        print("short")
+        goalType = .ShortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
+    }
+
+    @IBAction func longTermButtonWasPressed(_ sender: Any) {
+        print("long")
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
+    }
+
+    @IBAction func nextButtonWasPressed(_ sender: Any) {
+        print("next")
     }
 }
--- your goals/Extensions/UIButtonExtension.swift
+++ your goals/Extensions/UIButtonExtension.swift
@@ -1,11 +1,21 @@
 import UIKit
 
 extension UIButton {
+
     func setSelectedColor() {
-        self.backgroundColor = UIColor(red: 109, green: 188, blue: 99, alpha: 1)
+        self.backgroundColor = UIColor.red
+        // UIColor.init(red: 109, green: 188, blue: 99, alpha: 0)
+        // UIColor(red: 109, green: 188, blue: 99, alpha: 1)
     }
 
     func setDeselectedColor() {
-        self.backgroundColor = UIColor(red: 178, green: 221, blue: 175, alpha: 1)
+        self.backgroundColor = UIColor.blue
+        // UIColor.init(red: 178, green: 221, blue: 175, alpha: 0)
+        // UIColor(red: 178, green: 221, blue: 175, alpha: 1)
     }
 }
+
+/// List of the Bugs:
+///     1- the UIbutton are not changing the background colors when it's being clicked.
+///     2- the nextButton is not moving with Keyboard and the keyborad is not showing at all :)
+///     3- the app craches when I try to write or put the focus on textField.
