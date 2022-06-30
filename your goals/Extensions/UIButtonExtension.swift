5d821b0e68162afcf0b23015ca3875faa054e561
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-30 15:20:38 +0200

    Fix the UIbutton colors issue

--- your goals/Extensions/UIButtonExtension.swift
+++ your goals/Extensions/UIButtonExtension.swift
@@ -3,15 +3,11 @@ import UIKit
 extension UIButton {
 
     func setSelectedColor() {
-        self.backgroundColor = UIColor.red
-        // UIColor.init(red: 109, green: 188, blue: 99, alpha: 0)
-        // UIColor(red: 109, green: 188, blue: 99, alpha: 1)
+        self.backgroundColor = UIColor(red: 109/255, green: 188/255, blue: 99/255, alpha: 1)
     }
 
     func setDeselectedColor() {
-        self.backgroundColor = UIColor.blue
-        // UIColor.init(red: 178, green: 221, blue: 175, alpha: 0)
-        // UIColor(red: 178, green: 221, blue: 175, alpha: 1)
+        self.backgroundColor = UIColor(red: 178/255, green: 221/255, blue: 175/255, alpha: 1)
     }
 }
 
