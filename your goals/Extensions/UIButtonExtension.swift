b1dae0e11702c651c233c833b276d5ac02cc5a0e
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-01 09:07:33 +0200

    Refactor

--- your goals/Extensions/UIButtonExtension.swift
+++ your goals/Extensions/UIButtonExtension.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 extension UIButton {
-
     func setSelectedColor() {
         self.backgroundColor = UIColor(red: 109/255, green: 188/255, blue: 99/255, alpha: 1)
     }
@@ -12,6 +11,4 @@ extension UIButton {
 }
 
 /// List of the Bugs:
-///     1- the UIbutton are not changing the background colors when it's being clicked.
-///     2- the nextButton is not moving with Keyboard and the keyborad is not showing at all :)
-///     3- the app craches when I try to write or put the focus on textField.
+///     1- the nextButton is not moving with Keyboard and the keyborad is not showing at all :) and the app craches when I try to write or put the focus on textField.
