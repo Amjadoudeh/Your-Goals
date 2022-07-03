3d7f32ded1fbad529e4efdbc8c4ec4e379281959
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-03 23:18:48 +0200

    Save data to core data

--- your goals/Controller/FinishGoalViewController.swift
+++ your goals/Controller/FinishGoalViewController.swift
@@ -21,7 +21,13 @@ class FinishGoalViewController: UIViewController {
     }
 
     @IBAction func createGoalButtonWasPressed(_ sender: Any) {
-        // Pass data into Core Data Goal Model
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
 
     @IBAction func backButtonWasPressed(_ sender: Any) {
@@ -29,6 +35,21 @@ class FinishGoalViewController: UIViewController {
     }
 
     func save(completion: (_ finished: Bool) -> Void) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
 
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+
+        do {
+            try managedContext.save()
+            print("successed")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
     }
 }
