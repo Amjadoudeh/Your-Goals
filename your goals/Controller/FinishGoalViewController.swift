f50562f0c43f2c2c0ccec7ea14ca6a9e5e619cf2
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-03 10:43:02 +0200

    Create FinishGoalViewController

---
+++ your goals/Controller/FinishGoalViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FinishGoalViewController.swift
+//  your goals
+//
+//  Created by Amajd Oudeh on 03.07.22.
+//
+
+import UIKit
+
+class FinishGoalViewController: UIViewController {
+
+    @IBOutlet weak var createGoalButton: UIButton!
+
+    @IBOutlet weak var pointsTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func createGoalButtonWasPressed(_ sender: Any) {
+        // Pass data into Core Data Goal Model
+    }
+}
