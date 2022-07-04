210c67d9213c1c0da713dd8cc25b71e419bd89dc
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-04 16:31:52 +0200

    List some bugs

--- your goals/Extensions/UIButtonExtension.swift
+++ your goals/Extensions/UIButtonExtension.swift
@@ -12,3 +12,5 @@ extension UIButton {
 
 /// List of the Bugs:
 ///     1- the nextButton is not moving with Keyboard and the keyborad is not showing at all :) and the app craches when I try to write or put the focus on textField.
+///     2- replacement of UITableViewRowAction because it was deprecated in iOS 13.0: Use UIContextualAction and related APIs instead.
+///     3- fix the Cell UI and test the app by adding / deleting goals and increase the progress and see the changes.
